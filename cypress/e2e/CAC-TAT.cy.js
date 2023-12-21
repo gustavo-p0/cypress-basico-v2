77b75442be536c67f8b868c2937a713652c357e7
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2023-12-20 21:40:44 -0300

    exexrcicio 01

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -6,4 +6,15 @@ describe("Central de atendimento ao Cliente TAT", () => {
   it("verifica o titulo da aplicação", () => {
     cy.title().should("eq", "Central de Atendimento ao Cliente TAT");
   });
+  // exercício 01
+  it("Preenche os campos obrigatórios e envia o formulário", () => {
+    cy.get("#firstName").type("Dutch");
+    cy.get("#lastName").type("Salamandra");
+    cy.get("#email").type("email@email.com");
+    cy.get("#open-text-area").type(
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis et aliquid vero pariatur odit quas! Cum, sit vero odit maiores corporis veniam necessitatibus temporibus ab quis incidunt assumenda laboriosam dolorem?"
+    );
+    cy.get(`button.button[type = "submit"]`).click();
+    cy.get(`span.success`).should("be.visible");
+  });
 });
